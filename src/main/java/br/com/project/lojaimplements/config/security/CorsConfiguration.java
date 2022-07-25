aad34e9f405ae611fc71207f066e6b8451a43fae
brunovinicius <brunoviniciustica@gmail.com>
2022-07-25 11:19:22 -0300

    Implementando algumas nomodificacoes no kors

--- src/main/java/br/com/project/lojaimplements/config/security/CorsConfiguration.java
+++ src/main/java/br/com/project/lojaimplements/config/security/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        	.allowedOrigins("https://carango-bom-front-react.herokuapp.com")
+        	.allowedOrigins("http://localhost:3000")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 
